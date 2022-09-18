8338faf40ec8b6c5f283b44c9093537aec4677ca
Nonobo <nonobenor@gmail.com>
2022-09-18 13:29:38 +0300

    Added fonts created mainscreen

--- Hermes/Hermes/MainActivity.cs
+++ Hermes/Hermes/MainActivity.cs
@@ -1,6 +1,8 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.OS;
 using Android.Runtime;
+using Android.Widget;
 using AndroidX.AppCompat.App;
 
 namespace Hermes
@@ -8,12 +10,17 @@ namespace Hermes
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private Button toSignIn, toSignUp;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+            toSignIn = FindViewById<Button>(Resource.Id.mainactivity_to_signin);
+            toSignUp =FindViewById<Button>(Resource.Id.mainactivity_to_signup);
+            toSignUp.Click += delegate { Intent i = new Intent(this, typeof(SignUpActivity)); StartActivity(i); };
+            toSignIn.Click += delegate { Intent i = new Intent(this, typeof(SignInActivity)); StartActivity(i); };
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
